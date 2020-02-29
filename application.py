8bc472a1faf27fdc389d0370e509169619ced478
Sobhan Mondal <30654420+sobhaniiest@users.noreply.github.com>
2020-02-29 13:32:17 +0530

    modified

--- application.py
+++ application.py
@@ -1,6 +1,4 @@
 import time
-
-# Start with a basic flask app webpage.
 from flask_socketio import SocketIO, emit
 from flask import Flask, render_template, url_for, copy_current_request_context
 from random import random
@@ -13,10 +11,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 app.config['DEBUG'] = True
 
-#turn the flask app into a socketio app
 socketio = SocketIO(app, async_mode=None, logger=True, engineio_logger=True)
-
-#random number Generator Thread
 thread = Thread()
 thread_stop_event = Event()
 
@@ -31,12 +26,10 @@ def get_data():
 
 @app.route('/')
 def index():
-    #only by sending this page first will the client be connected to the socketio instance
     return render_template('index.html')
 
 @socketio.on('connect', namespace='/test')
 def test_connect():
-    # need visibility of the global thread object
 	global thread
 	print('Client connected')
 	global fp
