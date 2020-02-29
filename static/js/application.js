ac5cc47636066ae5455281589596407ac6756040
Sobhan Mondal <30654420+sobhaniiest@users.noreply.github.com>
2020-02-29 13:33:19 +0530

    modified

--- static/js/application.js
+++ static/js/application.js
@@ -1,13 +1,9 @@
 
 $(document).ready(function(){
-    //connect to the socket server.
     var socket = io.connect('http://' + document.domain + ':' + location.port + '/test');
     var numbers_received = [];
 
-    //receive details from server
     socket.on('newnumber', function(msg) {
-        console.log("Received number" + msg.number);
-        //maintain a list of ten numbers
         if (numbers_received.length >= 10){
             numbers_received.shift()
         }            
